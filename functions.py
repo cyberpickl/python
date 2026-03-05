7a14be6b8bb9aa9ac3c7971600a065cd8230b078
shaina doraiswamy <shainadoraiswamy2002@outlook.com>
2026-03-05 10:17:46 +0530

    Added comments in functions.py - progress

--- functions.py
+++ functions.py
@@ -1,14 +1,15 @@
 
+#input fields for collecting name and place
 name = input("Hi What's your name?: ")
 place = input ("Where do you live?: ")
 
 
 
-
+#creating a function that takes name and place variables as parameters
 def greet (name, place):
     print(f"Hello {name}")
     print(f"How do you do!You live in {place}")
     print ("Isn't the weather nice")
 
-
+#calling the function and passing arguments 
 greet(name, place)
