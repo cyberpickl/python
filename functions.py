02f37d4efd23d1790b2988dff7782abf77587eba
shaina doraiswamy <shainadoraiswamy2002@outlook.com>
2026-03-05 10:01:42 +0530

    first commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+name = input("Hi What's your name?: ")
+place = input ("Where do you live?: ")
+
+
+
+
+def greet (name, place):
+    print(f"Hello {name}")
+    print(f"How do you do!You live in {place}")
+    print ("Isn't the weather nice")
+
+
+greet(name, place)
