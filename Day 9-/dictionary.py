17cd3d510db74f92fd1acbc6b2e00df2349dd418
shaina doraiswamy <shainadoraiswamy2002@outlook.com>
2026-03-05 16:46:01 +0530

    new mini project to pratice dictionaries, listed.

---
+++ Day 9-/dictionary.py
@@ -0,0 +1,15 @@
+#Dictionaries : Key: Value pairs within curly brackets
+
+programming_dict = {
+    "Bug": "an error, flaw, or fault in a computer program that causes it to produce incorrect, unexpected, or unintended results.",
+    "Function": "a named, reusable block of code that performs a specific task."
+
+}
+
+# print(programming_dict["Bug"])
+
+
+programming_dict["Loop"] = "a control flow statement that allows a block of code to be executed repeatedly until a specific condition is met or for a predefined number of times"
+empty_dict ={}
+programming_dict = empty_dict
+print(programming_dict)
